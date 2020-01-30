ab8c7ee1894af2f945939fca5bdedbdaeaa506d8
Armandus <armando.duran.correa@gmail.com>
2020-01-29 18:42:57 -0600

    USO de debug

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 // REQUIRE
 var express = require('express');
 var morgan = require('morgan')
-
+const config = require("config");
 
 // declaraciones
 var app = express();
-const config = require("config");
 const app_tipo = config.get("app.tipo");
+const debug_main = require("debug")("app:main");
+const debug_second = require("debug")("app:second");
 
 // Middleware
 app.use(morgan('tiny'));
 
 // Mensaje de configuracion Principal
 console.log("Tipo de APP  =" + app_tipo);
-
+debug_main("Habilitado Debug=app:main");
+debug_second("Habilitado Debug=app:second");
 
 
 
