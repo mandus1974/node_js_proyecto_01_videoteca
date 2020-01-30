3066bc9a2f40802c93142720c01033cbc208f979
Armandus <armando.duran.correa@gmail.com>
2020-01-29 18:55:20 -0600

    Ejemplo de @hapi/joi.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var morgan = require('morgan')
 const config = require("config");
+const Joi = require("@hapi/joi");
+
 
 // declaraciones
 var app = express();
@@ -11,6 +13,20 @@ const debug_second = require("debug")("app:second");
 
 // Middleware
 app.use(morgan('tiny'));
+
+
+/// SHEMAS
+
+const schema = Joi.object({
+  nombre:Joi.string().min(3).required()
+});
+
+const {error, value} = schema.validate({nombre:"juan"});
+if(error)
+  console.log(error);
+else
+  console.log(value, " VALIDO !!");
+
 
 // Mensaje de configuracion Principal
 console.log("Tipo de APP  =" + app_tipo);
